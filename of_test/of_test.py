c0dc3966ffae618132d19b112e9c57e4d9eae072
liquidcronos <jan-baumgaertner@web.de>
2018-10-03 11:46:33 +0200

    added cluster view for bad features

--- of_test/of_test.py
+++ of_test/of_test.py
@@ -20,6 +20,29 @@ def tomuchvel(newpos,oldpos,maxspeed,dist_to_cam):
     return immobile[:,:,1]*immobile[:,:,0]
     
 
+def findcluster(bad,numb_of_cluster):
+
+    #generate empty list for each cluster
+    clusterlist=[]
+
+    #find clusters using kmean
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    bad=np.float32(bad)
+    ret,label,center=cv2.kmeans(bad,numb_of_cluster,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+        
+    
+    #color for each cluster
+    colorcl = np.random.randint(0,255,(numb_of_cluster,3))
+    
+    #seperate into different cluster
+    for i in range(numb_of_cluster):
+        newcluster=bad[label.ravel()==i]
+        clusterlist.append(newcluster)
+    #return array of clusters
+    return(np.array([np.array(cluster) for cluster in clusterlist]))
+
+
+
 
 cap = cv2.VideoCapture('traffic_saigon.avi')
 #cap = cv2.VideoCapture('highway.avi')
@@ -52,33 +75,38 @@ color = np.random.randint(0,255,(max_ft_numb,3))
 ret, old_frame = cap.read()
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 p0 = cv2.goodFeaturesToTrack(old_gray, mask = None ,useHarrisDetector=False, **feature_params)
+#TODO there should be a mask that excludes the outer perimiter of the image as those points are not fit for tracking
 
 
 pfirst=p0
+
+bad = np.empty([1,2])
 distsum=np.zeros((len(p0),1,2))
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
 
 
-first=True
+
+
+
 while(1):
     ret,frame = cap.read()
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # calculate optical flow
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
   
-    if first==True:
-        print(p1)
-        first=False
+    
+   
+  
       
     #classification of "good" features 
     good=tomuchvel(p1,pfirst,max_dist,1)
     #good,distsum=displsum(p1,p0,max_dist,distsum)
-    good_vel = tomuchvel(p1,p0,max_vel,1)     #use height estimation
+    good_vel = tomuchvel(p1,p0,max_vel,1)     #TODO use height estimation
 
 
     #number of features tracked
-    print(len(p0))
+    #print(len(p0))
     
     # Select good points
     good_new   =     p1[good_vel*good*st==True] #and good==1
@@ -86,6 +114,7 @@ while(1):
     good_first = pfirst[good_vel*good*st==True]
     #good_sum   = distsum[good*st==True]
 
+    bad = np.append(bad,p1[good_vel*good*st==False],axis=0)
 
     # draw the tracks 
     for i,(new,old) in enumerate(zip(good_new,good_old)):
@@ -93,12 +122,45 @@ while(1):
         c,d = old.ravel()
         mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
         frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1) 
-    img = cv2.add(frame,mask)
-    cv2.imshow('frame',img)
-    
+        img = cv2.add(frame,mask)
+        cv2.imshow('frame',img)
+   
+
+    #find new features if to few:
+    if len(p1)<= 70:   #sets minimum number of features 
+        clusterlist =findcluster(bad,4)
+       
+
+
+
+
+
+
+
+
+        #draw clusters for easy debugging
+        #iterate over all clusters...
+        masks=np.empty(len(clusterlist),dtype=object)
+        for i in range(len(clusterlist)): 
+            features=clusterlist[i]
+            #mask to draw clusters on
+            clustermask=np.zeros_like(old_frame)
+            
+            
+
+            #... and draw each point for each cluster 
+            for j in range(len(features)):  
+                masks[i] = cv2.circle(clustermask,(features[j,0],features[j,1]),5,color[i].tolist(),-1)  
+                #cv2.imshow('cluster_image'+str(i),cv2.add(frame,clustermask))
+
+        cv2.imshow('cluster_image',cv2.add(frame,masks[1])) #change [1] for different clusters
+
+        
+
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1,1,2)
