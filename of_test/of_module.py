36c7398003d3a0f084a2bf895c9fa9084fb5ad5f
liquidcronos <jan-baumgaertner@web.de>
2018-10-18 15:39:54 +0200

    split functions in seperate library

--- of_test/of_module.py
+++ of_test/of_module.py
@@ -1,42 +1,10 @@
 import numpy as np
 import cv2
-
-
-def immobile(newpos,oldpos,firstpos,maxspeed,maxdist):
-    speed_const= (np.abs(newpos-oldpos))<(maxspeed)
-    dist_const = (np.abs(newpos-firstpos))<(maxdist)
-    out_of_frame=(oldpos)!=np.array([1000,1000])  #check if point is substitution
-    return (speed_const*dist_const*out_of_frame)[:,:,1]*(speed_const*dist_const*out_of_frame)[:,:,0]
-
-def visualize(frame,mask,good_new,good_old):
-    red=[0,0,255] #TODO color as input
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-        mask = cv2.line(mask, (a,b),(c,d),red,  2)
-        frame = cv2.circle(frame,(a,b),5,red,-1)
-        img = cv2.add(frame,mask)
-        cv2.imshow('frame',img)
+import of_library as of
 
 
 
-#old clusterfinding module
-def findcluster(bad,numb_of_cluster):
-                                                                  
-     #generate empty list for each cluster
-     clusterlist=[]
- 
-     #find clusters using kmean
-     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-     bad=np.float32(bad)
-     ret,label,center=cv2.kmeans(bad,numb_of_cluster,None,criteria    ,10,cv2.KMEANS_RANDOM_CENTERS)
-           
-     #seperate into different cluster
-     for i in range(numb_of_cluster):
-         newcluster=bad[label.ravel()==i]
-         clusterlist.append(newcluster)
-     #return array of clusters
-     return(np.array([np.array(cluster) for cluster in clusterlist    ]))
+
 
 
 #finding clusters using hierachical clustering (in c++ this is part of opencv)
@@ -48,30 +16,28 @@ def findcluster(bad,numb_of_cluster):
 def newfeature(bad_points,good_points,immobile):
     good_points=good_points[immobile==True]
     moving_ft=np.ones_like(old_frame) 
+    bad_reshaped=bad_points[:,0,:]
     if len(bad_points) >1:
 
         #crutch which scales clusternumber with point number
         clusternumb=max([3,int(len(bad)/80)])
-        clusterlist=findcluster(bad_points,clusternumb)
-       
-        #drawing bounding boxes around moving clusters
-        for cluster in clusterlist:
-            rect=cv2.minAreaRect(cluster)
-            box=cv2.boxPoints(rect)
-            box=np.int0(box)
-            cv2.drawContours(moving_ft,[box],0,0,cv2.FILLED)
+        #generate clusters of moving features using kmean
+        clusterlist=of.kmeancluster(bad_points,clusternumb)
+        #drawing bounding boxes around clusters
+        of.boundingboxes(clusterlist,moving_ft,feature_params["minDistance"])
+
     elif len(bad_points) == 1:
-        bad_reshaped=bad_points[:,0,:]
+        #append to good points to draw circle
         good_points=np.append(good_points,bad_reshaped,axis=0)
+
+
     #drawing circles around immobile points
-    for points in good_points:
-        x=int(points[0])
-        y=int(points[1])
-        cv2.circle(moving_ft,(x,y),feature_params["minDistance"],0,cv2.FILLED)
-    
+    of.circles(good_points,moving_ft,feature_params["minDistance"])
+
+
 
     moving_ft_gray=cv2.cvtColor(moving_ft,cv2.COLOR_BGR2GRAY)
-    
+
     #drawing mask for debugging
     cv2.imshow('mask',moving_ft_gray*255)
     
@@ -142,7 +108,7 @@ while(1):
 
      
     #select unmoving features
-    immobile_points=immobile(new_pos,old_pos,first_pos,max_vel,max_dist)*status
+    immobile_points=of.immobile(new_pos,old_pos,max_vel,1,[1000,1000])*status
     #print("pointnumber:",len(immobile_points))
     
     #generate new features if less than max
@@ -162,7 +128,7 @@ while(1):
     
         
         #visualize
-        visualize(frame,mask,new_pos,old_pos)
+        of.visualize(frame,mask,new_pos,old_pos,"frame")
 
 
     k = cv2.waitKey(30) & 0xff
