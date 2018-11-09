1bbee6374827164f6d80eeb5519801c57670460c
liquidcronos <jan-baumgaertner@web.de>
2018-11-09 13:18:39 +0100

    changed to homography based approach

--- of_test/of_module.py
+++ of_test/of_module.py
@@ -1,71 +1,15 @@
 import numpy as np
 import cv2
 import of_library as of
+import rospy
+import yaml
+import matplotlib.pyplot as plt
 
 
-
-def newfeature(bad_points,good_points,immobile):
-    good_points=good_points[immobile==True]
-    moving_ft=np.ones_like(old_frame) 
-    bad_reshaped=bad_points[:,0,:]
-
-
-    #how to handle moving features:
-    #---------------------------------------------------
-    '''
-    if len(bad_points) >1:
-        #crutch which scales clusternumber with point number
-        clusternumb=max([3,int(len(bad)/80)])
-        #generate clusters of moving features using kmean
-        clusterlist=of.kmeancluster(bad_points,clusternumb)
-        #drawing bounding boxes around clusters
-        of.boundingboxes(clusterlist,moving_ft,feature_params["minDistance"])
-    elif len(bad_points) == 1:
-        #append to good points to draw circle
-        good_points=np.append(good_points,bad_reshaped,axis=0)
-    '''
-    of.circles(bad_reshaped,moving_ft,feature_params["minDistance"]) #if only using  circles than bad points dont need to be saved
-
-
-
-    #how to handle unmoving features
-    #--------------------------------------------------------------
-    of.circles(good_points,moving_ft,feature_params["minDistance"])
-
-
-    #propably takes a long time and should be avoidet
-    moving_ft_gray=cv2.cvtColor(moving_ft,cv2.COLOR_BGR2GRAY)
-
-    #drawing mask for debugging
-    cv2.imshow('mask',moving_ft_gray*255)
-    
-    #adding new features
-    #---------------------------------------------------------
-    new_params=feature_params
-    new_params["maxCorners"]=int(ft_numb-np.sum(immobile))
-    features=cv2.goodFeaturesToTrack(frame_gray, mask = moving_ft_gray , **new_params)
-
-
-    #handling case that not enoigh new features where found
-    #---------------------------------------------------------------
-    missing_feat=int(new_params["maxCorners"]-len(features[:,0,0]))
-    if new_params["maxCorners"]!=len(features[:,0,0]):
-           print("boop")
-           features=np.append(features,1000*np.ones((missing_feat,1,2)),axis=0)
-
-
-
-    return features
 
 #parameter------------------------------
 #TODO calculate parameters based on height
-max_ft_numb=100        #maximum number of features
-height=100             #height above ground
-#TODO height as array of length max_ft_numb
-
-#TODO generate those from imu and height
-max_dist=30 
-max_vel=8
+max_ft_numb=250        #maximum number of features
 
 
 feature_params = dict( maxCorners = max_ft_numb,                                    
@@ -75,23 +19,28 @@ feature_params = dict( maxCorners = max_ft_numb,
 
 # Parameters for lucas kanade optical flow
 lk_params = dict( winSize  = (4,4),   #changed from (15,15)
-                  maxLevel = 1,       #changed from 2
+                  maxLevel = 3,       #changed from 1
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.5)) #changed from 10,0.03
- 
+
 #------------------------------------------------------
+
+#placeholder values for later measurments-------------
+d=100  #later calculatet of height measure and dot product
+n=np.array([0,0,1])
+
+#-----------------------------------
+
+
 
 
 cap=cv2.VideoCapture("traffic_saigon.avi")
-#cv2.VideoCapture(0) for live Camerafeed
-
 
 #read first frame and detect features
 ret, old_frame=cap.read()
 old_gray=cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)  #convert to grayscale
 
 
-#mask for areas excluded from Feature Tracking
-#TODO draw Border around image which depends on height
+#TODO Tiling to distribute points evenly over image
 old_pos = cv2.goodFeaturesToTrack(old_gray, mask =None, **feature_params)
 
 #TODO later includet in new initalisation function
@@ -99,39 +48,78 @@ old_pos_err=np.empty((1,2))
 #number of features used for tracking
 ft_numb=len(old_pos[:,0,0])
 
+#save first feature position
 first_pos=old_pos
-bad=np.empty([1,1,2])
 
-#mask for visualization
+#initiate velocity as 0
+v_old=np.zeros(3)
+
+#initialize d vector
+d=np.ones(len(old_pos))*d
+
+#mask to visualize feasible points
 mask=np.zeros_like(old_frame)
 while(1):
-    ret,frame = cap.read() #read next frame...
+    ret,frame = cap.read()
     frame_gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #... and convert to gray
-    new_pos, status, new_pos_err = cv2.calcOpticalFlowPyrLK(old_gray,frame_gray,old_pos,None,**lk_params)
+    new_pos, status, new_pos_err = cv2.calcOpticalFlowPyrLK(old_gray,frame_gray,old_pos,None,**lk_params) 
+    #reshape into proper 2dim array
+    new_pos=new_pos.reshape((len(new_pos),2))
+    old_pos=old_pos.reshape((len(old_pos),2))
 
-     
-    #select unmoving features
-    immobile_points=of.static_immobile(new_pos,old_pos,max_vel,1,[1000,1000])*status
     
-    if sum(immobile_points) < ft_numb:
-        #generate new features 
-        new_points=newfeature(bad,first_pos,immobile_points)
-        
-        #replace old features with new ones
-        new_points=new_points.reshape(-1,2)
-        old_pos[immobile_points==False]=new_points
-        new_pos[immobile_points==False]=new_points
-        first_pos[immobile_points==False]=new_points
-         
-        #track removed features
-        bad=np.append(bad,old_pos[immobile_points==False].reshape(-1,1,2),axis=0)  
-        bad=np.append(bad,first_pos[immobile_points==False].reshape(-1,1,2),axis=0)
+
+
+
+
+    #generate x according to math for simple implementation (not fast or efficient)
+    x=np.ones((len(new_pos),3))
+    x[:,:-1]=new_pos
+
+    #generate u according to math for simple implementation (not fast or efficient)
+    u=np.zeros((len(new_pos),3))
+    u[:,:-1]=new_pos-old_pos   #TODO what do we do if we loose features in the meantime?
+    #u=u.flatten()
+
+
+
+    #TODO calculate u in [m]
+
+
+    #TODO normally this would use the IMU data and a internal drone modell
+    v_new=v_old+np.random.normal(0,0.01,3)
+
+    # measure how well features fit the assumptions
+    feasibility,distance=of.r_tilde(x,u,n,v_new) 
     
-        
-        #visualize
-        of.visualize(frame,mask,new_pos,old_pos,"frame")
+    #delete values where row of vectors exede feasibility 
+    feasible_new=x[feasibility>=0.7]
+    feasible_flow=u[feasibility>=0.7]
+    feasible_dist=d[feasibility>=0.7]
+    # clusters need to be found and parallel planes need to be tracked ( the difference between d shoudl remain equal) 
 
+    
+    #construct LGS to solve for speed using optained d and features
+    
+    A= np.empty((0,3))
+    B= np.empty(0)
+    for i in range(len(feasible_new)):
+        ai=np.array([[0,-1,feasible_new[i,1]],[1,0,-feasible_new[i,0]],[-feasible_new[i,1],feasible_new[i,0],0]])/d[i]
+        bi=np.dot(ai,feasible_flow[i])/np.dot(n,feasible_new[i])
+        A=np.append(A,ai,axis=0)
+        B=np.append(B,bi)
+    v_obs,R,rank,s=np.linalg.lstsq(A,B)
+    
 
+    #implement kalman filter to fuse both measurments
+
+    #visual test-------------------------------------------
+    of.visualize(frame,mask,new_pos[feasibility>=0.3],old_pos[feasibility>=0.3],"test")
+    of.visualize(frame,mask,new_pos[feasibility>=0.6],old_pos[feasibility>=0.6],"test",marker=[0,255,0])
+    of.visualize(frame,mask,new_pos[feasibility>=0.9],old_pos[feasibility>=0.9],"test",marker=[255,0,0])
+    #-----------------------------------------------------------
+
+    
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
--- of_test/stamp.py
+++ of_test/stamp.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 
 daten=of.read_yaml_imu('BeispielDatenImuCam22-10-18/imuData.yaml')
 
+#print(daten)
 timestamb=[]
 for data in daten:
     timestamb.append(data[0])
 
-#plt.hist(timestamb,bins=100)
-plt.scatter(np.arange(0,len(timestamb)),timestamb)
+#print(timestamb)
+plt.hist(timestamb,bins=100)
+#plt.scatter(np.arange(0,len(timestamb))[0:100],timestamb[0:100])
 plt.show()
